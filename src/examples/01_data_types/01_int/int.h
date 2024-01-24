d1276efa9d1d19f50565b27b1762c59d8a04c3e1
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-01-24 00:54:31 +0000

    code, test, and main program w int test case

--- src/examples/01_data_types/01_int/int.h
+++ src/examples/01_data_types/01_int/int.h
@@ -1,1 +1,3 @@
-//Create prototype for void function echo_variable with one int parameter
+//Create prototype for int function echo_variable with one int parameter
+
+int echo_variable(int num);//prototype,header, signature
