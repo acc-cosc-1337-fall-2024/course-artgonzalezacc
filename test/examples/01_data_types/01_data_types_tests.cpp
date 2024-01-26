c04ee2fcf912b9d9af562d5e7ba85c496983c389
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-01-26 00:41:32 +0000

    double inconsistency adding

--- test/examples/01_data_types/01_data_types_tests.cpp
+++ test/examples/01_data_types/01_data_types_tests.cpp
@@ -1,23 +1,32 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "int.h"
+#include "decimals.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
 TEST_CASE("Verify echo variable", "verification") {
-	REQUIRE(echo_variable(10) == 10);
+	REQUIRE(echo_variable(10) == 11);
 }
 
 
 /*test case echo_variable */
 
 /*test case add_to_double_1 with 0 as parameter*/
+TEST_CASE("Verify add to double 1 w 0 as parameter", "show issues when comparing double(decimal) values") {
+	//REQUIRE(add_to_double_1(0) == .9);
+}
+
 
 /*test case add_to_double_1 with 1 as parameter*/
 
 /*test case add_to_double_2 with 0 as parameter*/
+TEST_CASE("Verify add to double 2 w 0 as parameter", "show issues when comparing double(decimal) values") {
+	REQUIRE(add_to_double_2(0) == 1.5);
+}
+
 
 /*test case add_to_double_2 with 1 as parameter*/
 
