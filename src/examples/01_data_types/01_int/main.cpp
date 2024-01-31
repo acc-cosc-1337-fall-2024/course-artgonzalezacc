db056f4eafcfa51d723f89f4f6fe05ef8b95d20a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-01-31 00:56:06 +0000

    use sizeof to determine data type size

--- src/examples/01_data_types/01_int/main.cpp
+++ src/examples/01_data_types/01_int/main.cpp
@@ -7,5 +7,17 @@ int main()
 {
     cout<<"Echo: "<<echo_variable(10)<<"\n";
 
+    auto num = 10;
+
+    cout<<"Bytes required to store int: "<<sizeof(num)<<"\n";
+
+    auto decimal = 10.5;
+
+    cout<<"Bytes required to store double: "<<sizeof(decimal)<<"\n";
+
+    auto ch = 'A';
+
+    cout<<"Bytes required to store char: "<<sizeof(ch)<<"\n";
+
     return 0;
 }
