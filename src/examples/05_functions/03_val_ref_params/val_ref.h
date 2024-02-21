e54ed4782e927557674c14bad88b8d58651d73fc
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-02-21 03:41:09 +0000

    function const reference params

--- src/examples/05_functions/03_val_ref_params/val_ref.h
+++ src/examples/05_functions/03_val_ref_params/val_ref.h
@@ -1,2 +1,3 @@
-void value_params(int num);
-void reference_params(int& num);
+void value_params(int num);//value 
+void reference_params(int& num);//reference
+void const_reference_param(const int& num);//protect data w const-read only
