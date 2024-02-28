135184909b40310dedde793f6d164bcdead5a7b9
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-02-28 00:17:51 +0000

    create a string

--- src/examples/06_strings/01_create/main.cpp
+++ src/examples/06_strings/01_create/main.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
+#include<string>
 
-using std::cout;
+using std::cout; using std::string;
 
 int main()
 {
-    char letter = 'a';
-
-    cout<<letter<<"\n";
+    string lang = "C++";
+    
+    cout<<lang<<"\n";
+    cout<<"Size of string: "<<lang.size()<<"\n";
 
     return 0;
 }
