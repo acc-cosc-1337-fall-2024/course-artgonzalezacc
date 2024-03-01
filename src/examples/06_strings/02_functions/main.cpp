f701022dca22dc8a1a5a9315e129b557a547cd03
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-03-01 00:58:48 +0000

    create strings

--- src/examples/06_strings/02_functions/main.cpp
+++ src/examples/06_strings/02_functions/main.cpp
@@ -4,11 +4,21 @@ using std::string;
 
 int main()
 {
-    string lang = "C++";
+    string s0 = "C++ is cool";
+    cout<<s0<<"\n";
+
+    string s1(s0, 4, 7);
+    cout<<s1<<"\n";
+
+    string s2("another character sequence", 12);
+    cout<<s2<<"\n";
+
+    string s3(10, 'x');
+    cout<<s3<<"\n";
+
+    string s4(10, 42);//10 *
+    cout<<s4<<"\n";
     
-  //  display_str_for(lang);
-    //display_for_ref(lang);
-    display_for_range(lang);
 
     return 0;
 }
