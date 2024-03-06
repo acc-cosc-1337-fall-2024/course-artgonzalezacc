0285edcd4723722624c1fcb8edbeac2eb6cdb5d5
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-03-06 00:23:09 +0000

    vector creation

--- src/examples/07_vector/01_create/main.cpp
+++ src/examples/07_vector/01_create/main.cpp
@@ -1,24 +1,31 @@
 #include<iostream>
+#include<string>
 #include<vector>
 
-using std::cout; using std::vector;
+using std::cout; using std::vector; using std::string;
 
 int main()
 {
     vector<int> nums {7, 2, 9};//create and initialize a vector
-    cout<<nums[1]<<"\n";
+    cout<<nums[1]<<"\n\n";
 
-    for(auto i=0; i < nums.size(); i++)
-    {
-        cout<<nums[i]<<"\n";
-    }
+    vector<int> nums1;
+    nums1.push_back(5);
+    nums1.push_back(0);
 
-    cout<<"\n";
-
-    for(auto num: nums)
+    for(auto num: nums1)
     {
         cout<<num<<"\n";
     }
+
+    cout<<"\n\n";
+
+    vector<string> str{9, "X"};
+    for(auto i=0; i < str.size(); i++)
+    {
+        cout<<str[i]<<"\n";
+    }
+
 
     return 0;
 }
