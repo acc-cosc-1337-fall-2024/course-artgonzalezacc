133f1571a4ea7bfb141a5bbe7da151c48c91bebc
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-03-21 23:39:26 +0000

    add default  constructor

--- src/examples/08_classes/01_bank/bank_account.h
+++ src/examples/08_classes/01_bank/bank_account.h
@@ -5,7 +5,9 @@ class BankAccount
 {
 
 public: //other regions of code, main, test, other function, another class can see the get_balance
+    BankAccount(){}//with no parameters-default synthesized constructor
     BankAccount(int b) : balance(b)  {/*empty class function/method block*/ }
+
     int get_balance(){return balance;}
 
 private://only BankAccount can read/write to the balance directly
