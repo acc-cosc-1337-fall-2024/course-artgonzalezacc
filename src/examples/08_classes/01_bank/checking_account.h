592dc88edd13d74e2dd76c6ff7506fbd102a7756
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-03-28 23:37:16 +0000

    Inheritance Checking Account

--- src/examples/08_classes/01_bank/checking_account.h
+++ src/examples/08_classes/01_bank/checking_account.h
@@ -1,1 +1,16 @@
 //checking_account.h
+#include "bank_account.h"
+
+
+#ifndef CHECKING_ACCOUNT_H
+#define CHECKING_ACCOUNT_H
+
+class CheckingAccount: public BankAccount
+{
+public:
+    CheckingAccount(){/*empty code block*/}
+    CheckingAccount(int b) : BankAccount(b) {/*empty code block*/}
+
+};
+
+#endif
