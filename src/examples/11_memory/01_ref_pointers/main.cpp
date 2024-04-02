b1a1f0b28b56b139f882f6ca3b673da24078fb70
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-04-02 23:29:12 +0000

    reference review, intro to pointer

--- src/examples/11_memory/01_ref_pointers/main.cpp
+++ src/examples/11_memory/01_ref_pointers/main.cpp
@@ -1,8 +1,28 @@
 #include "ref_pointers.h"
 #include<iostream>
 
+using std::cout;
+
 int main() 
 {
+	int num1 = 5;
+	int& num1_ref = num1;
+
+	cout<<num1_ref<<"\n";
+	num1_ref = 10;
+
+	cout<<num1_ref<<"\n";
+
+	cout<<num1<<"\n";
+
+	int num2 = 5;
+	int* num2_ptr = &num2;
+	cout<<*num2_ptr<<"\n";
+
+	*num2_ptr = 10;
+	cout<<*num2_ptr<<"\n";
+	cout<<num2<<"\n";
+
 	
 	return 0;
 }
