e299e2f1faa46a6a205db3c46859e43410b30160
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-04-02 23:57:58 +0000

    allocate memory with new and pointer

--- src/examples/11_memory/01_ref_pointers/main.cpp
+++ src/examples/11_memory/01_ref_pointers/main.cpp
@@ -5,15 +5,14 @@ using std::cout;
 
 int main() 
 {
-	int num1 = 5, num2 = 10;
-	int* num_ptr; //don't have to point to amything
-	num_ptr = &num1;
-	cout<<"Address of num1: "<<&num1<<"\n";
-	cout<<"Address that num_ptr points to: "<<num_ptr<<"\n";
-	cout<<"Value of the variable num_ptr points to: "<<*num_ptr<<"\n";
+	//create memory
+	int* num = new int(5);//create memory on the heap
 
-	num_ptr = &num2;
-	cout<<*num_ptr<<"\n";
+	//use memory
+	cout<<*num<<"\n";
+
+	//delete/free memory
+	delete num;
 
 	
 	return 0;
