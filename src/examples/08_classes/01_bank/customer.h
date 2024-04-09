621665c93e1a82242eb862d5b5658fd04db6b33f
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-04-09 23:52:06 +0000

    atm example with unique ptrs; and customer class

--- src/examples/08_classes/01_bank/customer.h
+++ src/examples/08_classes/01_bank/customer.h
@@ -1,1 +1,25 @@
 //customer.h
+#include<memory>
+#include<string>
+#include<vector>
+
+#include "bank_account.h"
+#include "checking_account.h"
+#include "savings_account.h"
+
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+class Customer
+{
+public:
+    Customer(int id, std::string n);
+    std::unique_ptr<BankAccount>& get_account(int index){return accounts[index];}
+    
+private:
+    int customer_id;
+    std::string name;
+    std::vector<std::unique_ptr<BankAccount>> accounts;
+};
+
+#endif
