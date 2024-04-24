0bec85436cf70ea0975b9251bfd9ef00899eeb38
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-04-24 03:21:36 +0000

    Use stack arrays

--- src/examples/10_arrays/01_arrays_mem/arrays_mem.h
+++ src/examples/10_arrays/01_arrays_mem/arrays_mem.h
@@ -1,3 +1,9 @@
 //arrays_mem.h - stack array example
+#include<iostream>
+#include<iomanip>
+
+const auto COLS = 10;
+const auto ROWS = 10;
 
 //function prototype for stack_array
+void use_stack_array();
