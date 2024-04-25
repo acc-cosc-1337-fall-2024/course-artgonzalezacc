93b33d7b33971badbf5886105fc428233b292180
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-04-25 23:59:26 +0000

    overload [] for my vector

--- src/examples/11_memory/06_vector/main.cpp
+++ src/examples/11_memory/06_vector/main.cpp
@@ -4,10 +4,13 @@
 using std::cout;
 
 int main()
-{
+{   
     Vector v(3);
+    v[0] = 5;
+
     cout<<"Size is: "<<v.Size()<<"\n";
     cout<<"Capacity is: "<<v.Capacity()<<"\n";
+    cout<<v[0]<<"\n";
     return 0;
 }
 
