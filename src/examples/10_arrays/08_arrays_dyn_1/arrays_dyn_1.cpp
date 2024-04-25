903397d021ee4f27928e5ccf1dd72503996a5a9f
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-04-25 23:21:43 +0000

    shared ptr to manage dynamic memory w pointer and new

--- src/examples/10_arrays/08_arrays_dyn_1/arrays_dyn_1.cpp
+++ src/examples/10_arrays/08_arrays_dyn_1/arrays_dyn_1.cpp
@@ -1,6 +1,6 @@
 #include "arrays_dyn_1.h"
 
-using std::cout;
+using std::cout; using std::shared_ptr;
 
 void limit_dynamic_memory_scope(const int SIZE)
 {
@@ -10,4 +10,27 @@ void limit_dynamic_memory_scope(const int SIZE)
     cout<<nums[0]<<"\n";
     
     delete[] nums;
+}
+
+int* get_dynamic_memory(const int SIZE)
+{
+    int* nums = new int[SIZE];
+    cout<<"Memory allocation: "<<nums<<"\n";
+    return nums;
+}
+
+void delete_dynamic_memory(int* nums)
+{
+    cout<<"Deleting memory allocation: "<<nums<<"\n";
+    delete[] nums;
+}
+
+void use_dynamic_memory(const int size)
+{
+    //allocate memory
+    shared_ptr<int[]> nums(get_dynamic_memory(size), delete_dynamic_memory);
+    nums[0] =  5;
+    cout<<nums[0]<<"\n";
+
+    cout<<"Exiting the use dynamic memory function\n";
 }
