fb1b4b1754d54f886d7242094c479f4a34d3fb3a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-04-30 23:18:21 +0000

    my vector copy constructor

--- src/examples/11_memory/06_vector/my_vector.cpp
+++ src/examples/11_memory/06_vector/my_vector.cpp
@@ -8,9 +8,27 @@ Vector::Vector(int s)
     cout<<"Memory allocated at: "<<elements<<"\n";
 }
 
+/**
+ Copy Constructor
+ 1-Copy size from v1 to v2
+ 2-Copy capacity from v1 to v2
+ 3-Create new memory for v2 
+ 4-Copy the values from v1 to v2
+*/
+Vector::Vector(const Vector& v1) 
+: size{v1.size}, capacity{v1.capacity}, elements{new int[v1.size]}
+{
+    cout<<"Memory allocated in Copy Constructor "<<elements<<"\n";
+
+    for(auto i=0; i < v1.size; i++)
+    {
+        elements[i] = v1.elements[i];
+    }
+}
+
 Vector::~Vector()
 {
-    delete[] elements;
     cout<<"Deallocate memory at: "<<elements<<"\n";
+    delete[] elements;
 }
 
