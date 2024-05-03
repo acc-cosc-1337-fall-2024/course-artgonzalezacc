8f5b14c83b5c5229d01a94d6ec574ca87547f60f
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-05-03 00:16:15 +0000

    recursive factorial

--- src/examples/12_recursion/01_recursion/recursion.cpp
+++ src/examples/12_recursion/01_recursion/recursion.cpp
@@ -3,11 +3,43 @@
 using std::cout;
 
 //Write code for recursive display function
-void say_hello()
+void say_hello(int num)
 {
+
+    if(num == 0)
+    {
+        cout<<"Base case\n";
+        return;
+    }
+
     cout<<"Hello\n";
 
-    say_hello();
+    cout<<"Load to stack\n";
+    say_hello(num-1);
+    cout<<"Unload from stack\n";
 }
 
 //Write code for recursive factorial
+//Write code for recursive factorial
+/*
+5*4*3*2*1=
+*/
+int recursive_factorial(int num)
+{
+    int f;//track num * r
+    int r; //track return value of function
+
+    if(num == 0)
+    {
+        cout<<"Base case\n";
+        return 1;
+    }
+
+    cout<<"Load to stack "<<num-1<<"\n";
+    r = recursive_factorial(num-1);
+    //execute once the base case is reaached///unwinding/unloading functions from the stack
+    f = num * r;
+    cout<<"Unload from stack "<<num<<" r "<<r<<" f "<<f<<"\n";
+
+    return f;
+}
--- src/examples/12_recursion/01_recursion/recursion.h
+++ src/examples/12_recursion/01_recursion/recursion.h
@@ -1,7 +1,8 @@
 #include<iostream>
 
-void say_hello();
+void say_hello(int num);
 //Write prototype for void function display with one int parameter
 
 //Write prototype for int value-return function with one int parameter
 
+int recursive_factorial(int num);
