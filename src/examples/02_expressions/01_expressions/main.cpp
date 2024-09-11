e9158abdda3dbefd62b0ac68f16e0b3803e25a9b
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-09-11 23:54:20 +0000

    char and string

--- src/examples/02_expressions/01_expressions/main.cpp
+++ src/examples/02_expressions/01_expressions/main.cpp
@@ -1,5 +1,15 @@
+#include<iostream>
+#include<string>
+
+using std::cout; 
+using std::string;
 
 int main()
 {
+    char letter = 'a';
+    cout<<letter<<"\n";
+
+    string lang = "Python";
+
     return 0;
 }
