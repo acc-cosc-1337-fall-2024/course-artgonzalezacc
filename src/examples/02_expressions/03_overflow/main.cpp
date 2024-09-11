e04d4a8edfc370cf264a673ae8ea4748f8b6d015
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-09-11 23:39:23 +0000

    overflow and underflow

--- src/examples/02_expressions/03_overflow/main.cpp
+++ src/examples/02_expressions/03_overflow/main.cpp
@@ -1,6 +1,20 @@
+#include<iostream>
+#include<limits.h> //access to INT_MAX
 
-int main()
+using std::cout; using std::cin;
+
+int main() //overflow
 {
+    int max_int = INT_MAX;
+    //ADD 1 to max_int
+    cout<<"Value of max_int: "<<max_int<<"\n";
+    
+    max_int += 1;
+    cout<<"Value of max_int: "<<max_int<<"\n";
+
+    //underflow
+    float floatVar = 3.0E-47; //C++ can't represent a number that's too close to 0
+    cout<<"Value of floatVar: "<<floatVar<<"\n";
 
     return 0;
 }
