315a1235e3d3fb85a0c214909ef49b56591cd51a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-09-25 23:09:28 +0000

    into do while

--- src/examples/04_repetition/03_do_while/do_while.cpp
+++ src/examples/04_repetition/03_do_while/do_while.cpp
@@ -1,7 +1,21 @@
 #include "do_while.h"
 
+using std::cout; using std::cin;
+
 //Write code for void function prompt_user to loop until
-//user opts not to continue.  
+//user opts not to continue. 
+void prompt_user()
+{
+    auto choice = 'Y';
+
+    do
+    {
+        cout<<"Enter Y or y to continue...";
+        cin>>choice;
+
+    } while (choice == 'Y' || choice == 'y');
+    
+} 
 
 
 
