3e954f724905ba520b26aac313a91ee5a38c1453
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-10-02 23:23:31 +0000

    intro to reference operator

--- src/examples/05_functions/02_references/main.cpp
+++ src/examples/05_functions/02_references/main.cpp
@@ -1,4 +1,27 @@
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    int num = 5;
+    int num1 = 10;
+    int num2 = 20;
+    char letter = 'a';
+
+    cout<<num<<"\n";
+    cout<<"num is stored at memory address: "<<&num<<"\n";
+
+    cout<<num1<<"\n";
+    cout<<"num1 is stored at memory address: "<<&num1<<"\n";
+
+    cout<<num2<<"\n";
+    cout<<"num2 is stored at memory address: "<<&num2<<"\n";
+
+
+    cout<<sizeof(num)<<"\n";
+    cout<<sizeof(letter)<<"\n";
+
+
     return 0;
 }
---
+++ src/examples/05_functions/02_references/reference.h
@@ -0,0 +1,1 @@
+//& reference -- memory operator
