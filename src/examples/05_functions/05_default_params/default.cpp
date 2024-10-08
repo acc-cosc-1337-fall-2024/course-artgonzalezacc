934b363703f8b88c16242a49130560ecef87123e
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-10-08 00:01:26 +0000

    function overloading

--- src/examples/05_functions/05_default_params/default.cpp
+++ src/examples/05_functions/05_default_params/default.cpp
@@ -9,3 +9,18 @@ double get_total1(int qty, double price, double bonus)
 {
     return qty * price + bonus;
 }
+
+double get_gross_pay(int hours, double rate)
+{
+    return hours * rate;
+}
+
+double get_gross_pay(int hours, double rate, double bonus)
+{
+    return hours * rate + bonus;
+}
+
+double get_gross_pay(double yearly_pay)
+{
+    return yearly_pay / 26;
+}
--- src/examples/05_functions/05_default_params/default.h
+++ src/examples/05_functions/05_default_params/default.h
@@ -1,2 +1,6 @@
 double get_total(int qty = 5, double price = 10);
 double get_total1(int qty=5, double price = 10, double bonus = 100);
+//overloading
+double get_gross_pay(int hours, double rate);
+double get_gross_pay(int hours, double rate, double bonus);
+double get_gross_pay(double yearly_pay);
