83cef8b03a9d92bdf226554dd6ec52b3fcea8c3b
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-10-09 23:08:39 +0000

    create a string

--- src/examples/06_strings/01_create/main.cpp
+++ src/examples/06_strings/01_create/main.cpp
@@ -1,4 +1,15 @@
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::string;
+
 int main()
 {
+    auto num = 0;
+    char letter = 'c'; //arrays
+    
+    string lang = "C++";
+
     return 0;
 }
