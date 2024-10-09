30458b1ed85cb4861defb9898f3b0882ef8f26e4
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-10-09 23:26:56 +0000

    append to string beyond capacity; new memory granted

--- src/examples/06_strings/01_create/main.cpp
+++ src/examples/06_strings/01_create/main.cpp
@@ -6,10 +6,33 @@ using std::string;
 
 int main()
 {
-    auto num = 0;
-    char letter = 'c'; //arrays
-    
     string lang = "C++";
+
+    cout<<"size of lang: "<<lang.size()<<"\n";
+    cout<<"capacity of lang: "<<lang.capacity()<<"\n";
+
+    lang.append("1");
+    lang.append("2");
+    lang.append("3");
+    lang.append("4");
+    lang.append("5");
+
+    lang.append("6");
+    lang.append("7");
+    lang.append("8");
+    lang.append("9");
+    lang.append("0");
+
+    lang.append("1");
+    lang.append("2");
+
+    cout<<"size of lang: "<<lang.size()<<"\n";
+    cout<<"capacity of lang: "<<lang.capacity()<<"\n";
+
+    lang.append("3");
+
+    cout<<"size of lang: "<<lang.size()<<"\n";
+    cout<<"capacity of lang: "<<lang.capacity()<<"\n";
 
     return 0;
 }
