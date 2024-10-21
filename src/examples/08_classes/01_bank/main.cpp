0bd0d643e580166679bb4bb3eea4fe6f920b9dac
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-10-21 23:53:01 +0000

    use account in main

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,9 +1,20 @@
 #include "bank_account.h"
 #include <ctime> //guarantees a random number
 
+using std::cin; using std::cout;
+
 int main()
 {
-	Account account;
+	auto balance = 0;
+	
+	cout<<"Enter amount: ";
+	cin>>balance;
+
+	Account account(balance);//represents a customer account
+
+	cout<<account.get_balance()<<"\n";
+
+	Account account1(500);//represents another customer's account
 
 	return 0;
 }
