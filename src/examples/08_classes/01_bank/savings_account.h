844f0b42d696dc5f213fc953410825e152c49bc9
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-10-30 23:20:56 +0000

    savings get balance override

--- src/examples/08_classes/01_bank/savings_account.h
+++ src/examples/08_classes/01_bank/savings_account.h
@@ -9,6 +9,7 @@ class SavingsAccount: public Account
 public:    
     SavingsAccount(){}
     SavingsAccount(int b): Account(b){}
+    int get_balance() const{return Account::get_balance() + (Account::get_balance() * .05 / 12);}
 };
 
 #endif
