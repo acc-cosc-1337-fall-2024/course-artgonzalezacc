d9656098e5eefe2c333dd8c2be8a8c71bd5e8528
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-10-31 00:02:40 +0000

    main atm use vector of accounts

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,16 +1,18 @@
 #include "atm.h"
+#include "checking_account.h"
+#include "savings_account.h"
+#include<vector>
 
 using std::cin; using std::cout; 
+using std::vector;
 
 int main()
 {
-	Account& account = get_account_by_reference();
-
-	display_balance(account);
-
-	cout<<"Main: "<<account.get_balance()<<"\n";
-
-/*	Account account(1000);
+	auto account_index = 0;
+	vector<Account> accounts = {CheckingAccount(), SavingsAccount()};
+	cout<<"1 - Checking 2 - Savings";
+	cin>>account_index;
+	Account account = accounts[account_index - 1];
 	ATM atm(account);
 
 	atm.display_balance();
@@ -21,7 +23,7 @@ int main()
 	atm.make_withdraw();
 	atm.display_balance();
 
-	std::cout<<account.get_balance()<<"\n";*/
+	std::cout<<account.get_balance()<<"\n";
 
 	return 0;
 }
