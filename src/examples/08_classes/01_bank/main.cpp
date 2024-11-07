f9c449dc1f3876f5edf501d33586df04bb4af895
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-11-07 00:42:20 +0000

    shared ptr example

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -5,12 +5,17 @@
 #include<memory>
 
 using std::cin; using std::cout; 
-using std::vector; using std::unique_ptr; using std::make_unique;
+using std::vector; using std::shared_ptr; using std::make_shared;
 
 int main()
 {
-	unique_ptr<int> num = make_unique<int>(10);
-	cout<<*num<<"\n";
+
+	shared_ptr<int> num_sp1 = make_shared<int>(10);
+	cout<<*num_sp1<<"\n";
+	cout<<num_sp1.use_count()<<"\n";
+	shared_ptr<int> num_sp2 = num_sp1;
+	cout<<*num_sp2<<"\n";
+	cout<<num_sp2.use_count()<<"\n";
 
 /*	vector<Account> accounts = {CheckingAccount(), SavingsAccount()};
 	cout<<"1 - Checking 2 - Savings";
