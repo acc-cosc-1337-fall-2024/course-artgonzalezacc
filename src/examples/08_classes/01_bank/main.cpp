46114ef8c0a117aa757130d1eb430fd85a1fea50
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-11-07 00:31:59 +0000

    unique ptr example

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -2,17 +2,15 @@
 #include "checking_account.h"
 #include "savings_account.h"
 #include<vector>
+#include<memory>
 
 using std::cin; using std::cout; 
-using std::vector;
+using std::vector; using std::unique_ptr; using std::make_unique;
 
 int main()
 {
-	int* num = new int(10);
+	unique_ptr<int> num = make_unique<int>(10);
 	cout<<*num<<"\n";
-	delete num;
-	num = nullptr;
-
 
 /*	vector<Account> accounts = {CheckingAccount(), SavingsAccount()};
 	cout<<"1 - Checking 2 - Savings";
