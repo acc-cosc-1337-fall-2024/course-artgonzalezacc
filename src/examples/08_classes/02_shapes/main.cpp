4eb38bbf0c57f743edffeedb756ca51f1e439d65
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-11-19 00:40:02 +0000

    vector of legacy pointers

--- src/examples/08_classes/02_shapes/main.cpp
+++ src/examples/08_classes/02_shapes/main.cpp
@@ -14,17 +14,21 @@ using std::vector;
 
 int main() 
 {
-	Line line;
-	Circle circle;
-
 	vector<Shape*> shapes;
-	shapes.push_back(&line);
-	shapes.push_back(&circle);
+	shapes.push_back(new Line());//create Line on the heap
+	shapes.push_back(new Circle());//create Circle on the heap
 
 	for(auto shape: shapes)
 	{
 		shape->draw();
 	}
+
+	for(auto shape: shapes)
+	{
+		delete shape;
+		shape = nullptr;
+	}
+
 
 	return 0;
 }
